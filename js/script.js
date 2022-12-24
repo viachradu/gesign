31263cdbb0b6d6f62f6bea746086f6ba740da53f
viachradu <93644723+viachradu@users.noreply.github.com>
2022-12-24 18:13:01 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+window.addEventListener('load', windowLoad);
+
+function windowLoad() {
+	document.addEventListener('click', documentActions);
+}
+
+function documentActions(e) {
+	const targetElement = e.target;
+
+	// Scroll To...
+	if (targetElement.hasAttribute('data-goto')) {
+		const gotoElement = document.querySelector(`${targetElement.dataset.goto}`);
+		const headerHeight = document.querySelector(`.header`).offsetHeight;
+
+		if (gotoElement) {
+			window.scrollTo({
+				top: gotoElement.offsetTop - headerHeight,
+				behavior: "smooth"
+			});
+		}
+
+		e.preventDefault();
+	}
+
+	// Works filter
+	if (targetElement.classList.contains('items-works__type') && !targetElement.classList.contains('active')) {
+		const activeElement = document.querySelector(`.items-works__type.active`);
+		const elements = document.querySelectorAll(`.items-works__item`);
+		const elementType = targetElement.dataset.workType;
+
+		activeElement.classList.remove('active');
+		targetElement.classList.add('active');
+
+		elements.forEach(element => {
+			!elementType || element.dataset.workType === elementType ?
+				element.hidden = false : element.hidden = true;
+		});
+	}
+}
